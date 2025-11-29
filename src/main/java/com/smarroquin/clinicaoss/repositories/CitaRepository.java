cefe6f8061c5a49937f0ac2c0502c08e08851fe4
Sharon <168245948+Sharonanelisse@users.noreply.github.com>
2025-11-29 09:11:55 -0600

    Dashboard

--- src/main/java/com/smarroquin/clinicaoss/repositories/CitaRepository.java
+++ src/main/java/com/smarroquin/clinicaoss/repositories/CitaRepository.java
@@ -3,6 +3,7 @@ package com.smarroquin.clinicaoss.repositories;
 import com.smarroquin.clinicaoss.models.Cita;
 import jakarta.enterprise.context.ApplicationScoped;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @ApplicationScoped
@@ -16,6 +17,39 @@ public class CitaRepository extends BaseRepository<Cita, Long> {
     public List<Cita> findByPacienteId(Long pacienteId) {
         return getEm().createQuery("SELECT c FROM Cita c WHERE c.paciente.id = :pid ORDER BY c.fechaApertura DESC", Cita.class)
                 .setParameter("pid", pacienteId)
+                .getResultList();
+    }
+
+    // Contar citas en un rango
+    public Long contarCitas(LocalDateTime inicio, LocalDateTime fin) {
+        return getEm().createQuery("SELECT COUNT(c) FROM Cita c WHERE c.fechaApertura BETWEEN :inicio AND :fin", Long.class)
+                .setParameter("inicio", inicio)
+                .setParameter("fin", fin)
+                .getSingleResult();
+    }
+
+    // Contar citas canceladas
+    public Long contarCanceladas(LocalDateTime inicio, LocalDateTime fin) {
+        return getEm().createQuery("SELECT COUNT(c) FROM Cita c WHERE c.estado_cita = 'CANCELADA' AND c.fechaApertura BETWEEN :inicio AND :fin", Long.class)
+                .setParameter("inicio", inicio)
+                .setParameter("fin", fin)
+                .getSingleResult();
+    }
+
+    // Contar odontólogos distintos que han tenido citas
+    public Long contarOdontologosActivos(LocalDateTime inicio, LocalDateTime fin) {
+        return getEm().createQuery("SELECT COUNT(DISTINCT c.user) FROM Cita c WHERE c.fechaApertura BETWEEN :inicio AND :fin", Long.class)
+                .setParameter("inicio", inicio)
+                .setParameter("fin", fin)
+                .getSingleResult();
+    }
+
+    // Para GRÁFICA: Citas por Doctor
+// Retorna lista de arreglos: [NombreDoctor, CantidadCitas]
+    public List<Object[]> contarCitasPorDoctor(LocalDateTime inicio, LocalDateTime fin) {
+        return getEm().createQuery("SELECT c.user.nombreUsuario, COUNT(c) FROM Cita c WHERE c.fechaApertura BETWEEN :inicio AND :fin GROUP BY c.user.nombreUsuario", Object[].class)
+                .setParameter("inicio", inicio)
+                .setParameter("fin", fin)
                 .getResultList();
     }
 }
